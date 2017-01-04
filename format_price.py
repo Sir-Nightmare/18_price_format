09e574b6b2394633db81b5e4fc98a54d75dba46a
Sir Nightmare <ivantochilkin@gmail.com>
2017-01-04 05:29:18 +0300

    the function was made

--- format_price.py
+++ format_price.py
@@ -1,5 +1,26 @@
+import argparse
+import re
+
+
 def format_price(price):
-    pass
+    if not (isinstance(price, (int, float, str))):
+        raise TypeError('Price have to be int, float or str')
+    if isinstance(price, str):
+        price_pattern = re.compile(r'\d+[\.,]?\d*$')
+        if re.match(price_pattern, price):
+            price = price.replace(',', r'.')
+        else:
+            raise ValueError('Incorrect value')
+    price = float(price)
+    if price.is_integer():
+        return format(price, ',.0f').replace(',', ' ')
+    else:
+        return format(price, ',.2f').replace(',', ' ')
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        description='returns price in easy-readable format')
+    parser.add_argument('price', help='input price')
+    args = parser.parse_args()
+    print(format_price(args.price))
